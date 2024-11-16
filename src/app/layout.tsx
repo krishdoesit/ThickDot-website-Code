d3338ab33d9aa3bd4e6416b3aa416e47c588d54b
rishipatel2005 <madeintechnology95@gmail.com>
2024-11-16 14:35:01 +0530

    updated tailwind configuration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
                 <link rel="apple-touch-icon" sizes="180x180" href={"/favicons/apple-touch-icon.png"} />
                 <link rel="icon" type="image/png" sizes="32x32" href={"/favicons/favicon-32x32.png"} />
                 <link rel="icon" type="image/png" sizes="16x16" href={"/favicons/favicon-16x16.png"} />
-                <link rel="manifest" href={"/assets/favicons/site.webmanifest"} />
+                <link rel="manifest" href={"/favicons/site.webmanifest"} />
             </head>
             <body className={`${inter.className} overflow-x-hidden bg-white antialiased`}>
                 <Providers>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,7 +5,13 @@ export default {
     darkMode: "class",
     content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}", "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            willChange: {
+                transform: "transform",
+                opacity: "opacity",
+                "transform-opacity": "transform, opacity",
+            },
+        },
     },
     plugins: [
         nextui({
